12d5b89e3adc48467335433d325d0015023a813d
U-ma-s <yuma.prodia4989@gmail.com>
2023-03-04 08:58:56 +0900

    Initial Commit

---
+++ chefai/chefaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  chefaiApp.swift
+//  chefai
+//
+//  Created by 杉下友麻 on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct chefaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
